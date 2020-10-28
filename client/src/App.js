db1b187487d650a58dc1024908299cde5d0203c8
moscalir <moscalir@uni.coventry.ac.uk>
2020-10-28 20:50:59 +0000

    Create React Folder, Clean Up

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react'
+import Navbar from '../components/layout/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <Navbar/>
+
+      <h1>APP WELCOME</h1>
+      
+    </Fragment>
+
+  );
+}
+
+export default App;
