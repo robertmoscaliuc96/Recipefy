4860c8e27a18beb6e73e7c45f2830a959ff9bc12
moscalir <moscalir@uni.coventry.ac.uk>
2020-12-30 21:52:17 +0000

    Prepare To Deploy

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/auth', require("./routes/api/auth"));
 app.use('/api/profile', require("./routes/api/profile"));
  
 
-/* // Serve static assets in production 
+ // Serve static assets in production 
 if(process.env.NODE_ENV === 'production'){
 
     //Set static folder
@@ -29,7 +29,7 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))
     });
 }
- */
+
 
 const PORT = process.env.PORT || 5000;
 
